94426d61739011120cb986247e520281b6e25468
ar-junior <local.codesite@email.com>
2025-09-25 14:56:58 +0530

    arithmetic&math-q3

---
+++ arithmetic & math/basics.py
@@ -0,0 +1,37 @@
+# arithmetic
+
+x = 3.14
+y = 4
+z = 5
+
+# round off
+x1 = round(x)
+
+# absolute value....distance away from 0 as whole number.
+y1 = abs(y)
+
+# power 
+z1 = pow(z,3)
+
+# maximum & minimum
+a = max(x,y,z)
+b = min(x,y,z)
+
+print(a)
+print(b)
+
+# -------------
+
+# math
+
+import math
+
+x = 9.89
+
+a = math.pi
+b = math.e
+sq = math.sqrt(x)
+cl = math.ceil(x)
+fr = math.floor(x)
+
+print(fr)
---
+++ arithmetic & math/practice2.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("enter the radius of a circle : "))
+
+area = math.pi * pow(radius,2)
+
+print(f"the area of the circle is : {round(area,2)}cm^2")
