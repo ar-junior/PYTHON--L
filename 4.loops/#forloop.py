ac996728ecb80acaa5bbfdf2b61276093371c2e6
ar-junior <local.codesite@email.com>
2025-10-04 23:17:20 +0530

    nestedloop--Q9

---
+++ 4.loops/#forloop.py
@@ -0,0 +1,6 @@
+# for loops = execute a block of code a fixed number of times.
+#             You can iterate over a range, string, sequence, etc.
+
+        # iterable object
+for i in range(1,6):
+    print(i)    # Each iteration will run
---
+++ 4.loops/@clock.py
@@ -0,0 +1,12 @@
+import time
+
+user = int(input("Enter time in seconds : "))
+
+for x in range(user,-1,-1):
+    second = x % 60 
+    minute = int(x/60) % 60
+    hour = int(x/3600)
+    time.sleep(1)
+    print(f"{hour:02}:{minute:02}:{second:02}")
+
+print("Time's  up!!")
